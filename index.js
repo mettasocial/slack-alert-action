472a9becb16d4b3662926a6021a2c6cd1fbb4239
Nikhil Mehta <nikhil.mehta@mettasocial.com>
2020-11-05 21:34:58 +0530

    fix: debugging all action info and inputs

--- index.js
+++ index.js
@@ -4,39 +4,39 @@ const github = require("@actions/github");
 try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput("who-to-greet");
-  console.log(`Hello ${nameToGreet}!`);
+  core.info(`Hello ${nameToGreet}!`);
   const time = new Date().toTimeString();
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2);
-  console.log(
+  core.info(
     "SLACK_BUILD_MONITOR_CHANNEL_ID",
     core.getInput("SLACK_BUILD_MONITOR_CHANNEL_ID")
   );
-  console.log("SLACK_USER_TOKEN", core.getInput("SLACK_USER_TOKEN"));
-  console.log("SLACK_WBEHOOK_URL", core.getInput("SLACK_WBEHOOK_URL"));
-  console.log(
+  core.info("SLACK_USER_TOKEN", core.getInput("SLACK_USER_TOKEN"));
+  core.info("SLACK_WBEHOOK_URL", core.getInput("SLACK_WBEHOOK_URL"));
+  core.info(
     "CONTENTFUL_ENTITY_CONTENT_TYPE",
     core.getInput("CONTENTFUL_ENTITY_CONTENT_TYPE")
   );
-  console.log(
+  core.info(
     "CONTENTFUL_ENTITY_CONTENT",
     core.getInput("CONTENTFUL_ENTITY_CONTENT")
   );
-  console.log(`The event payload: ${payload}`);
-  console.log(
+  core.info(`The event payload: ${payload}`);
+  core.info(
     "payload.head_commit.committer.username",
     payload.head_commit.committer.username
   );
-  console.log("payload.head_commit.message", payload.head_commit.message);
-  console.log("payload.repository.full_name", payload.repository.full_name);
-  console.log("payload.ref", payload.ref);
-  console.log("github.context.eventName", github.context.eventName);
-  console.log("github.context.run_id", github.context.run_id);
-  console.log("fetch", fetch);
-  console.log("Date.now()", Date.now());
-  console.log(JSON.stringify(github.context, null, 2));
-  console.log(github.context.job.status);
-  //console.log(`The event payload: ${payload}`);
+  core.info("payload.head_commit.message", payload.head_commit.message);
+  core.info("payload.repository.full_name", payload.repository.full_name);
+  core.info("payload.ref", payload.ref);
+  core.info("github.context.eventName", github.context.eventName);
+  core.info("github.context.run_id", github.context.run_id);
+  core.info("fetch", fetch);
+  core.info("Date.now()", Date.now());
+  core.info(JSON.stringify(github.context, null, 2));
+  core.info(github.context.job.status);
+  //core.info(`The event payload: ${payload}`);
   core.setOutput("time", time);
 } catch (error) {
   core.setFailed(error.message);
