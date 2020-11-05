c5ea5625f4c3c8f24de3c83284ced5068d5edc77
Nikhil Mehta <nikhil.mehta@mettasocial.com>
2020-11-05 20:39:26 +0530

    feat: integrating slack alert

--- index.js
+++ index.js
@@ -1,15 +1,106 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+const core = require("@actions/core");
+const github = require("@actions/github");
+const fetch = require("node-fetch");
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
+function getenvNameByRef(ref) {
+  if (ref === "develop") {
+    return "development";
+  }
+  if (ref === "master" || ref === "main") {
+    return "staging";
+  }
+  if (ref.includes("production")) {
+    return "production";
+  }
 }
+
+async function sendMessage() {
+  try {
+    // create all variables
+    const { context } = github;
+    const { payload } = context;
+    const EVENT_NAME = context.eventName;
+    const SLACK_BUILD_MONITOR_CHANNEL_ID = core.getInput(
+      "SLACK_BUILD_MONITOR_CHANNEL_ID"
+    );
+    const SLACK_BUILDBOT_TOKEN = core.getInput("SLACK_BUILDBOT_TOKEN");
+    const SLACK_WBEHOOK_URL = core.getInput("SLACK_WBEHOOK_URL");
+    const JOB_STATUS = core.getInput("JOB_STATUS");
+    const CONTENTFUL_ENTITY_CONTENT_TYPE = core.getInput(
+      "CONTENTFUL_ENTITY_CONTENT_TYPE"
+    );
+    const CONTENTFUL_ENTITY_CONTENT = core.getInput(
+      "CONTENTFUL_ENTITY_CONTENT"
+    );
+    const GITHUB_REF = getenvNameByRef(payload.ref.split("/").pop());
+    const GITHUB_REPOSITORY = payload.repository.full_name.split("/").pop();
+    const GITHUB_RUN_ID = context.runId;
+    const COMMIT_MESSAGE = payload.head_commit.message.trim();
+    const GITHUB_ACTOR = payload.head_commit.committer.username;
+    //create basic template for json slack message
+    let jsonMessage = {
+      channel: SLACK_BUILD_MONITOR_CHANNEL_ID,
+      username: "Mettasocial Build bot",
+      icon_url: "https://ibb.co/nsDLmjH",
+      attachments: [
+        {
+          color: "#FF0000",
+          as_user: false,
+          pretext: `Last build is ${JOB_STATUS} on project ${GITHUB_REPOSITORY} / environment ${GITHUB_REF}`,
+          title: `Build is ${JOB_STATUS} on project ${GITHUB_REPOSITORY} / environment ${GITHUB_REF}`,
+          title_link: `https://github.com/${GITHUB_REPOSITORY}`,
+          fields: [],
+          footer: "Metta Social Solutions Pvt. Ltd.",
+          footer_icon:
+            "https://cdn.mettasocial.com/logos/64_without_name_color.png",
+          ts: Date.now(),
+        },
+      ],
+    };
+    // set color
+    if (JOB_STATUS.toLowerCase() === "success") {
+      jsonMessage.attachments[0].color = "#36A64F";
+    } else if (JOB_STATUS.toLowerCase() === "failure") {
+      jsonMessage.attachments[0].color = "#FF0000";
+    }
+    if (EVENT_NAME === "workflow_dispatch") {
+      jsonMessage.attachments[0].fields = [
+        {
+          title: `Title of changed Content: ${CONTENTFUL_ENTITY_CONTENT}`,
+          value: `Contentful content type changed : ${CONTENTFUL_ENTITY_CONTENT_TYPE}`,
+          short: false,
+        },
+      ];
+    } else {
+      jsonMessage.attachments[0].fields = [
+        {
+          title: "Last commit by",
+          value: GITHUB_ACTOR,
+          short: true,
+        },
+        {
+          title: "Last commit message Says",
+          value: COMMIT_MESSAGE,
+          short: true,
+        },
+      ];
+    }
+    jsonMessage.attachments[0].fields.push({
+      title: "Check the pipeline link below for more details",
+      value: `https://github.com/${GITHUB_REPOSITORY}/actions/runs/${GITHUB_RUN_ID}`,
+      short: false,
+    });
+    await fetch(`${SLACK_WBEHOOK_URL}`, {
+      headers: {
+        Authorization: `Bearer ${SLACK_BUILDBOT_TOKEN}`,
+        "Content-type": "application/json; charset=utf-8",
+      },
+      body: JSON.stringify(jsonMessage),
+      method: "POST",
+    });
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+sendMessage();
